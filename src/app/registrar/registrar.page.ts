71f7a5457f947fe68281f32e04f7f98c9582467f
Carlos Garizado <carlos.garizado@covinoc.com>
2024-11-11 12:43:20 -0500

    se realiza el login de inicio de secion y el formulario de registro

--- src/app/registrar/registrar.page.ts
+++ src/app/registrar/registrar.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-registrar',
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegistrarPage implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit() {
+  }
+
+  goToLogin() {
+    this.router.navigate(['/login']);
   }
 
 }
