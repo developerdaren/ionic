317d1b06808c7ef4ff42ade6adfc23fdb4f6fbcb
developerdaren <castrodaren@gmail.com>
2024-11-11 08:18:31 -0500

    ionic

---
+++ src/app/imagenes/imagenes.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-imagenes',
+  templateUrl: './imagenes.page.html',
+  styleUrls: ['./imagenes.page.scss'],
+})
+export class ImagenesPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
